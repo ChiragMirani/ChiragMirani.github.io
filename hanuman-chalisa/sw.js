0da753fb1aaacba40d3b3a7360cbd80d31b5a490
Chirag Mirani <chiragmirani@gmail.com>
2026-05-04 21:04:50 -0400

    Publish Hanuman Chalisa reader

---
+++ hanuman-chalisa/sw.js
@@ -0,0 +1,53 @@
+const CACHE = "hanuman-chalisa-v1";
+const SHELL = [
+  "./",
+  "./index.html",
+  "./about.html",
+  "./data.json",
+  "./static/styles.css",
+  "./manifest.webmanifest",
+  "./apple-touch-icon.png",
+  "./favicon-192.png",
+  "./favicon-512.png",
+  "./favicon-32.png",
+  "./favicon.ico",
+  "./social-preview.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(SHELL)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+  if (request.method !== "GET") return;
+
+  if (request.headers.get("accept") && request.headers.get("accept").includes("text/html")) {
+    event.respondWith(
+      fetch(request).then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(request, copy));
+        return response;
+      }).catch(() => caches.match(request).then((match) => match || caches.match("./")))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cached) => cached || fetch(request).then((response) => {
+      const copy = response.clone();
+      caches.open(CACHE).then((cache) => cache.put(request, copy));
+      return response;
+    }))
+  );
+});
